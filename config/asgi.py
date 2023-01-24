702fe05aa1d83db3c34b2cdd09118c046ac1301a
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-01-24 17:11:28 -0500

    initial commit

---
+++ config/asgi.py
@@ -0,0 +1,41 @@
+"""
+ASGI config for django chat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
+
+"""
+import os
+import sys
+from pathlib import Path
+
+from django.core.asgi import get_asgi_application
+
+# This allows easy placement of apps within the interior
+# django_chat directory.
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR / "django_chat"))
+
+# If DJANGO_SETTINGS_MODULE is unset, default to the local settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+# This application object is used by any ASGI server configured to use this file.
+django_application = get_asgi_application()
+# Apply ASGI middleware here.
+# from helloworld.asgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+
+# Import websocket application here, so apps from django_application are loaded first
+from config import routing  # noqa isort:skip
+
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa isort:skip
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": URLRouter(routing.websocket_urlpatterns),
+    }
+)

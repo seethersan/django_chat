e92c75f3386ace55cf003de99e54116491de7d66
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-01-30 13:02:17 -0500

    refactor url

--- django_chat/users/urls.py
+++ django_chat/users/urls.py
@@ -7,17 +7,10 @@ from django_chat.users.views import (
     user_update_view,
 )
 
-from django_chat.users.api.views import DoctorViewSet, PatientViewSet
-
-router = DefaultRouter()
-router.register(r"patients", PatientViewSet, basename="doctor")
-router.register(r"doctors", DoctorViewSet, basename="patient")
-
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-    path(r"", include(router.urls)),
 ]

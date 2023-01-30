cd43a3649151adae1da138b2813b4b2454092e37
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-01-30 12:59:25 -0500

    add urrl for doctor and oatient

--- config/api_router.py
+++ config/api_router.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from django_chat.users.api.views import UserViewSet
 from django_chat.chat.api.views import ConversationViewSet, MessageViewSet
+from django_chat.users.api.views import DoctorViewSet, PatientViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -12,6 +13,8 @@ else:
 router.register("users", UserViewSet)
 router.register("conversations", ConversationViewSet)
 router.register("messages", MessageViewSet)
+router.register("patients", PatientViewSet, basename="doctor")
+router.register("doctors", DoctorViewSet, basename="patient")
 
 
 app_name = "api"

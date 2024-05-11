a449ea23201c8ac30491e72bbc8ef982f51f13bd
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2024-05-11 01:40:09 -0500

    implement channels

--- django_chat/chatrooms/consumers.py
+++ django_chat/chatrooms/consumers.py
@@ -1,15 +1,38 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
+		self.room_name = self.scope['url_route']['kwargs']['room_name']
+		self.room_group_name = 'chat_%s' % self.room_name
 
-    def disconnect(self, close_code):
-        pass
+		await self.channel_layer.group_add(
+			self.room_group_name,
+			self.channel_name
+		)
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+		await self.accept()
 
-        self.send(text_data=json.dumps({"message": message}))
+	async def disconnect(self, close_code):
+		await self.channel_layer.group_discard(
+			self.room_group_name,
+			self.channel_name
+		)
+
+	async def receive(self, text_data):
+		text_data_json = json.loads(text_data)
+		message = text_data_json['message']
+
+		await self.channel_layer.group_send(
+			self.room_group_name,
+			{
+				'type': 'chat_message',
+				'message': message
+			})
+
+	async def chat_message(self, event):
+		message = event['message']
+
+		await self.send(text_data=json.dumps({
+			'message': message
+		}))
